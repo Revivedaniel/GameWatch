8a5d6943a22662bd542bba06ba06e3a8b93b4771
Revivedaniel <mlg.danielstark@gmail.com>
2021-09-25 12:48:44 -0700

    Created the Games model

---
+++ models/Games.js
@@ -0,0 +1,36 @@
+const { Model, DataTypes } = require('sequelize');
+const sequelize = require('../config/connection');
+
+class Games extends Model {};
+
+Games.init(
+    {
+        id: {
+            type: DataTypes.INTEGER,
+            allowNull: false,
+            primaryKey: true,
+            autoIncrement: true,
+          },
+          title: {
+            type: DataTypes.STRING,
+            allowNull: false,
+          },
+          genre: {
+            type: DataTypes.STRING,
+            allowNull: false,
+          },
+          description: {
+            type: DataTypes.STRING,
+            allowNull: false,
+          }
+    },
+    {
+        sequelize,
+        timestamps: false,
+        freezeTableName: true,
+        underscored: true,
+        modelName: 'user',
+      }
+);
+
+module.exports = Games;
