7bd52d1872f183e013c32fd4955ad2a70f766540
Revivedaniel <mlg.danielstark@gmail.com>
2021-09-26 13:38:12 -0700

    Linked seed data and bulk created seeds

--- seeds/seed.js
+++ seeds/seed.js
@@ -1,7 +1,11 @@
 const sequelize = require('../config/connection');
-const { User, Project } = require('../models');
+const User = require('../models/User')
+const Game = require('../models/Game')
+const Review = require('../models/Review')
 
-const userData = require('./userData.json');
+const userData = require('./userData.json');  
+const gameData = require('./gameData.json')
+const reviewData = require('./reviewData.json')
 
 const seedDatabase = async () => {
   await sequelize.sync({ force: true });
@@ -11,12 +15,9 @@ const seedDatabase = async () => {
     returning: true,
   });
 
-  for (const project of projectData) {
-    await Project.create({
-      ...project,
-      user_id: users[Math.floor(Math.random() * users.length)].id,
-    });
-  }
+  const games = await Game.bulkCreate(gameData);
+
+  const reviews = await Review.bulkCreate(reviewData)
 
   process.exit(0);
 };
