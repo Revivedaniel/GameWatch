f5a68685923d8db05046c193f416e60229633244
Revivedaniel <mlg.danielstark@gmail.com>
2021-09-27 18:57:01 -0700

    Cleaned up

--- models/index.js
+++ models/index.js
@@ -1,25 +1,7 @@
 const User = require('./User');
-const path = require('path')
-const hbs = require('hbs');
-const express = require('express');
 const Game = require('./Game');
 const Review = require('./Review')
-const app = express()
 
-app.set('views', path.join(__dirname))
-app.set('views engine', 'hbs')
-
-app.get('/', function(req, res) {
-    res.render('Home', {
-        array: ['One', 'Two', 'Three'],
-        message: 'Hello'
-    })
-})
- 
-app.listen(3000, function(error) {
-    if(error) throw error
-    console.log("Server says hi")
-})
 
 Game.hasMany(Review, {
     foreignKey: "game_id",
