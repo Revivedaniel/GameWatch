d363339c78965c6cb61de9ee2618c77c95adcf0f
Revivedaniel <mlg.danielstark@gmail.com>
2021-09-29 19:27:16 -0700

    Moved /:title route to homeRoutes

--- controllers/api/gameRoutes.js
+++ controllers/api/gameRoutes.js
@@ -10,14 +10,6 @@ router.get("/", async (req, res) => {
     }
 });
 
-router.get("/:title", async (req, res) => {
-    try {
-        //WORKING HERE recreate game model then continue here
-        const gameData = await Game.findOne({where: {slug: req.params.title}});
-        res.status(200).json(gameData)
-    } catch (err) {
-        res.status(500).json(err)
-    }
-})
+
 
 module.exports = router;
