3eb0bf4a1bb2739862e946a4943328237d633811
Revivedaniel <mlg.danielstark@gmail.com>
2021-10-01 07:29:53 -0700

    Started creating the IGDB link

--- controllers/homeRoutes.js
+++ controllers/homeRoutes.js
@@ -1,26 +1,69 @@
 const router = require('express').Router();
-const { Project, User } = require('../models');
+const Game = require('../models/Game');
 const withAuth = require('../utils/auth');
+const axios = require('axios');
+require('dotenv').config();
 
 router.get('/', async (req, res) => {
-  res.render("homepage")
+  res.render('homepage');
 });
 
 router.get('/infopage', (req, res) => {
   res.render('infopage');
 });
 
-router.get("/game/:title", async (req, res) => {
+router.get('/game/:title', async (req, res) => {
   try {
-      //WORKING HERE recreate game model then continue here
-      const gameData = await Game.findOne({where: {slug: req.params.title}});
+    const gameData = await Game.findOne({ where: { slug: req.params.title } });
 
-      const game = gameData.get({plain: true});
+    const game = gameData.get({ plain: true });
 
-      res.render('infopage', game)
+    res.render('infopage', game);
   } catch (err) {
-      res.status(500).json(err)
+    const gameData = await axios({
+      url: 'https://api.igdb.com/v4/games',
+      method: 'POST',
+      headers: {
+        Accept: 'application/json',
+        'Client-ID': `${process.env.client_id}`,
+        Authorization: `Bearer ${process.env.token}`,
+      },
+      data: `fields age_ratings, cover, name, slug, summary, genres; where slug = "${req.params.title}";`,
+    })
+      .then(async (response) => {
+        //working here trying to make a second api call to get the cover_id
+        res.status(200).json(response.data);
+        //create new game with this data
+        const newGame = 
+          //title as is
+          //genres loaded
+          //summary as is
+          //age_ratings loaded
+          //cover and width loaded
+          axios({
+            url: "https://api.igdb.com/v4/covers",
+            method: 'POST',
+            headers: {
+              Accept: 'application/json',
+              'Client-ID': `${process.env.client_id}`,
+              Authorization: `Bearer ${process.env.token}`,
+            },
+            data: `fields image_id,width; where id = ${response.data.cover}`
+          })
+            .then(coverRes => {
+                res.status(200).json(coverRes)
+            })
+            .catch(err => {
+                console.error(err);
+            });
+
+          ////images.igdb.com/igdb/image/upload/t_thumb/co2s5t.jpg
+          //slug as is
+      })
+      .catch((err) => {
+        res.status(404).json(err);
+      });
   }
-})
+});
 
 module.exports = router;
