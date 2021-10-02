7154adc1a3c34e1513acf579ef4eeb77c33ef330
Revivedaniel <mlg.danielstark@gmail.com>
2021-10-02 11:18:58 -0700

    First try updating review

--- controllers/api/reviewRoutes.js
+++ controllers/api/reviewRoutes.js
@@ -2,21 +2,35 @@ const router = require('express').Router();
 const Review = require('../../models/Review');
 
 router.post('/', async (req, res) => {
-    try {
-        req.body.user_id = req.session.user_id
-        const review = {
-            user_id: req.session.user_id,
-            ...req.body
-        }
+  try {
+    req.body.user_id = req.session.user_id;
+    const review = {
+      user_id: req.session.user_id,
+      ...req.body,
+    };
 
-        const reviewData = await Review.create(review)
-        res.status(200).json(reviewData);
-        console.log(reviewData)
-    } catch (err) {
-        res.status(400).json(err);
-        console.log(err)
-      }
-})
+    const reviewData = await Review.create(review);
+    res.status(200).json(reviewData);
+    console.log(reviewData);
+  } catch (err) {
+    res.status(400).json(err);
+    console.log(err);
+  }
+});
 
+router.put('/:id', async (req, res) => {
+  try {
+    const review = await Review.update(
+      {
+        stars: req.body.stars,
+        review: req.body.review,
+      },
+      { where: { id: req.params.id } }
+    );
+    res.status(200).json(review);
+  } catch (err) {
+    res.status(500).json(err);
+  }
+});
 
 module.exports = router;
