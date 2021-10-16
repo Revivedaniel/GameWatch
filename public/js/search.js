44734b44c4e7038bc8eca4fb25a4ebd6b3d62636
Revivedaniel <mlg.danielstark@gmail.com>
2021-10-15 22:27:04 -0700

    Added Enter event listener to search input

--- public/js/search.js
+++ public/js/search.js
@@ -1,10 +1,17 @@
 const search = document.querySelector('#search');
 const searchInput = search.querySelector("input");
-const searchButton = search.querySelector("button");
+const searchButton = search.querySelector("a");
 
 //Adding eventlistener to searchButton to change the searchInput value into slug form and set the window.location to the slug
 searchButton.addEventListener('click', (event) => {
     event.preventDefault();
     const slug = searchInput.value.replace(/\s+/g, '-').toLowerCase();
     window.location = `/game/${slug}`
-})
+});
+searchInput.addEventListener('keypress', (event) => {
+if (event.key === "Enter") {
+    event.preventDefault();
+    const slug = searchInput.value.replace(/\s+/g, '-').toLowerCase();
+    window.location = `/game/${slug}`
+}
+});
