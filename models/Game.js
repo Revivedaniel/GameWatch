bb3fc5b7aaeac02e2fb56663bdbea239684e3432
Revivedaniel <mlg.danielstark@gmail.com>
2021-12-11 22:31:11 -0800

    Summary now has max of 10k chars

--- models/Game.js
+++ models/Game.js
@@ -20,7 +20,7 @@ Game.init(
             allowNull: false,
           },
           summary: {
-            type: DataTypes.STRING(2000),
+            type: DataTypes.STRING(10000),
             allowNull: false,
           },
           age_ratings: {
